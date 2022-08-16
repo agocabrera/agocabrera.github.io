f9e9c38df141e926087c35c9580ac81c2264c932
Agustín <agocabrera@gmail.com>
2022-08-15 22:20:47 -0300

    Gestión de error getJSONData()

--- js/products.js
+++ js/products.js
@@ -1,10 +1,11 @@
 let currentProductsArray = [];
+let listContainer = document.getElementById("pro-list-container");
 
 function showProductsList() {
-    let htmlContentToAppend = "";
-    for (let i = 0; i < currentProductsArray.length; i++) {
-        let product = currentProductsArray[i];
-        htmlContentToAppend += `<div class="list-group-item list-group-item-action cursor-active">
+  let htmlContentToAppend = "";
+  for (let i = 0; i < currentProductsArray.length; i++) {
+    let product = currentProductsArray[i];
+    htmlContentToAppend += `<div class="list-group-item list-group-item-action cursor-active">
           <div class="row">
             <div class="col-3">
               <img src="${product.image}" alt="${product.description}" class="img-thumbnail">
@@ -18,17 +19,21 @@ function showProductsList() {
             </div>
           </div>
         </div>`;
-        document.getElementById("pro-list-container").innerHTML = htmlContentToAppend;
-    }
+    listContainer.innerHTML = htmlContentToAppend;
+  }
 }
 
 // Una vez cargado el documento:
 
 document.addEventListener("DOMContentLoaded", function () {
-    getJSONData(PRODUCTS_URL + "101" + EXT_TYPE).then(function (resultObj) {
-        if (resultObj.status === "ok") {
-            currentProductsArray = resultObj.data.products;
-            showProductsList();
-        }
-    })
+  getJSONData(PRODUCTS_URL + "101" + EXT_TYPE).then(function (resultObj) {
+    if (resultObj.status === "ok") {
+      currentProductsArray = resultObj.data.products;
+      showProductsList();
+    } else {
+      listContainer.innerHTML = "No se ha podido cargar el contenido.";
+      listContainer.style.marginTop = "100px";
+      listContainer.classList.add("alert", "alert-danger", "text-center");
+    }
+  })
 });
