652f9ccb08b618a4bacf1946dad686a8d0e164bc
Agustín <agocabrera@gmail.com>
2022-08-19 02:56:39 -0300

    Google sign in, bootstrap modal feedback

--- js/login.js
+++ js/login.js
@@ -1,21 +1,31 @@
+// Al iniciar sesión con Google:
+
+function handleCredentialResponse(response) {
+    window.location.href = "inicio.html";
+};
+
+// Al iniciar sesión con email/contraseña:
+
 document.getElementById("submit").addEventListener("click", function () {
     let loginOk = true;
-    let errorMsg = "Los siguientes campos no pueden estar vacíos: \n";
+    let modalContentToAppend = "<p>Por favor ingrese:</p><ul>";
 
     if (document.getElementById("email").value == "") {
         loginOk = false;
-        errorMsg += "- Email \n";
+        modalContentToAppend += "<li>Email</li>"
     }
 
     if (document.getElementById("password").value == "") {
         loginOk = false;
-        errorMsg += "- Contraseña";
+        modalContentToAppend += "<li>Contraseña</li>"
     }
+
+    modalContentToAppend += '</ul><button type="button" class="btn btn-primary float-end" data-bs-dismiss="modal">Cerrar</button>';
+    document.getElementById("modalBody").innerHTML = modalContentToAppend;
 
     if (loginOk) {
         window.location.href = "inicio.html";
     } else {
-        alert(errorMsg);
+        new bootstrap.Modal(document.getElementById("modal")).show();
     }
-
 });
